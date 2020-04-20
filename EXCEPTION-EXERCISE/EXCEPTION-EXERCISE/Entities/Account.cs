39db5ddac99ca3df7c6fad4d5cb89a8d6f9bc824
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-04-20 20:05:42 -0300

    Adicionando exercício de exceção

---
+++ EXCEPTION-EXERCISE/EXCEPTION-EXERCISE/Entities/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using EXCEPTION_EXERCISE.Entities.Exceptions;
+
+namespace EXCEPTION_EXERCISE.Entities
+{
+    class Account
+    {
+        private string _holder;
+        public int Number { get; private set; }
+        public double Balance { get; private set; }
+        public double WithDrawLimit { get; private set; }
+
+        public string Holder 
+        {
+            get { return _holder; }
+            set 
+            { 
+                if(value.Length > 0 && value != null)
+                {
+                    _holder = value;
+                }
+            }
+        }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void  withDraw(double amount)
+        {
+            if(Balance == 0)
+            {
+                throw new DomainException("You cannot withdraw because your balance is zero");
+            }
+
+            if(amount > WithDrawLimit)
+            {
+                throw new DomainException("You cannot withdraw because your amount is highest withdraw limit");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ EXCEPTION-EXERCISE/EXCEPTION-EXERCISE/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using EXCEPTION_EXERCISE.Entities;
+using EXCEPTION_EXERCISE.Entities.Exceptions;
+
+namespace EXCEPTION_EXERCISE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Enter account data: ");
+                Console.Write("Number: ");
+                int accoutNumber = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string accountHolder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw Limit: ");
+                double withDrawLimit = double.Parse(Console.ReadLine());
+
+                Account acc = new Account(accoutNumber, accountHolder, initialBalance, withDrawLimit);
+
+                Console.WriteLine("------------------------------------------");
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                acc.withDraw(amount);
+
+                Console.Write("New Balance: " + acc.Balance);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }catch(Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }        
+        }
+    }
+}
