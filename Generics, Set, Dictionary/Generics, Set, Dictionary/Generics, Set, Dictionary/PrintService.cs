2c74cc511b61a7f5a29cc5f908e63f335d076db0
Luiz Felipe <luizfelipedasilva678@gmail.com>
2020-08-06 22:12:26 -0300

    Generics, Set, Dictionary

---
+++ Generics, Set, Dictionary/Generics, Set, Dictionary/Generics, Set, Dictionary/PrintService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Generics__Set__Dictionary
+{
+    class PrintService<T>
+    {
+        private List<T> nums = new List<T>();
+
+       public void addValue(T num)
+       {
+            nums.Add(num);
+       }
+
+       public T first()
+       {
+            return nums.First();
+       }
+
+       public void print()
+       {
+            nums.ForEach(x => Console.Write("[" + x + "]"));
+       }
+    }
+}
---
+++ Generics, Set, Dictionary/Generics, Set, Dictionary/Generics, Set, Dictionary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace Generics__Set__Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<string> nums = new PrintService<string>();
+            
+            Console.WriteLine("How many values ? ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for(int i = 0; i< qtd; i++)
+            {
+                //int num = int.Parse(Console.ReadLine());
+                string nome = Console.ReadLine();
+                nums.addValue(nome);
+            }
+
+            nums.print();
+            Console.WriteLine();
+            Console.WriteLine("First: " + nums.first()); 
+             
+        }
+    }
+}
